11af6ea77117ff0e6d900ad037b35cb611250238
Fine0830 <fanxue0830@gmail.com>
2023-04-18 16:05:58 +0800

    feat: do request

--- src/datasource.ts
+++ src/datasource.ts
@@ -6,12 +6,18 @@ import {
   MutableDataFrame,
   FieldType,
 } from '@grafana/data';
+import { getBackendSrv } from '@grafana/runtime';
 
 import { MyQuery, MyDataSourceOptions } from './types';
 
+// proxy route
+const routePath = '/graphql';
 export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
+  url: string;
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
     super(instanceSettings);
+    // proxy url
+    this.url = instanceSettings.url || '';
   }
 
   async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
@@ -21,6 +27,8 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
 
     // Return a constant for each query.
     const data = options.targets.map((target) => {
+      // const query = defaults(target, defaultQuery);
+      // const dataQuery = getTemplateSrv().replace(query.queryText, options.scopedVars);
       return new MutableDataFrame({
         refId: target.refId,
         fields: [
@@ -31,6 +39,24 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     });
 
     return { data };
+  }
+  async doRequest(params?: Record<string, any>) {
+    // Do the request on proxy; the server will replace url + routePath with the url
+    // defined in plugin.json
+    const result = getBackendSrv().fetch({
+      method: 'POST',
+      url: `${this.url}${routePath}`,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      params
+      // params: {
+      //   query: "query queryServices($duration: Duration!,$keyword: String!) {\n    services: getAllServices(duration: $duration, group: $keyword) {\n      key: id\n      label: name\n      group\n    }\n  }",
+      //   variables: {"duration":{"start":"2023-04-17 1503","end":"2023-04-17 1603","step":"MINUTE"},"keyword":""},
+      // }
+    });
+    console.log(result);
+    return result;
   }
 
   async testDatasource() {
