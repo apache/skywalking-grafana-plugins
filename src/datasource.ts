ad1d2ca9ed77579fe49726088fad50355f6bed0f
Fine0830 <fanxue0830@gmail.com>
2023-04-20 18:58:08 +0800

    feat: format duration

--- src/datasource.ts
+++ src/datasource.ts
@@ -13,6 +13,11 @@ import { MyQuery, MyDataSourceOptions, DEFAULT_QUERY } from './types';
 
 // proxy route
 const routePath = '/graphql';
+enum TimeType {
+  MINUTE_TIME = "MINUTE",
+  HOUR_TIME = "HOUR",
+  DAY_TIME = "DAY",
+}
 export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   url: string;
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
@@ -26,6 +31,12 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     console.log(options);
     const from = range!.from.valueOf();
     const to = range!.to.valueOf();
+    const dates = this.timeFormat([new Date(from), new Date(to)]);
+    const duration = {
+      start: this.dateFormatStep(dates.start, dates.step),
+      end: this.dateFormatStep(dates.end, dates.step),
+      step: dates.step,
+    };
 
     // Return a constant for each query.
     const data = options.targets.map(async (target) => {
@@ -33,11 +44,11 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
       const dataQuery = getTemplateSrv().replace(query.queryText, options.scopedVars);
       const  s =  {
         query: "query queryServices($duration: Duration!,$keyword: String!) {\n    services: getAllServices(duration: $duration, group: $keyword) {\n      key: id\n      label: name\n      group\n    }\n  }",
-        variables: {"duration":{"start":"2023-04-17 1503","end":"2023-04-17 1603","step":"MINUTE"},"keyword":""},
+        variables: {duration,"keyword":""},
       };
       const t = {
         query: "query queryData($duration: Duration!, $serviceIds: [ID!]!) {\n  topology: getServicesTopology(duration: $duration, serviceIds: $serviceIds) {\n    nodes {\n      id\n      name\n      type\n      isReal\n    }\n    calls {\n      id\n      source\n      detectPoints\n      target\n    }\n  }}",
-        variables: {"duration":{"start":"2023-04-17 1503","end":"2023-04-17 1603","step":"MINUTE"},"serviceIds":["YWdlbnQ6OnNvbmdz.1","YWdlbnQ6OnJlY29tbWVuZGF0aW9u.1","YWdlbnQ6OmFwcA==.1","YWdlbnQ6OmdhdGV3YXk=.1","YWdlbnQ6OmZyb250ZW5k.1"]},
+        variables: {duration,"serviceIds":["YWdlbnQ6OnNvbmdz.1","YWdlbnQ6OnJlY29tbWVuZGF0aW9u.1","YWdlbnQ6OmFwcA==.1","YWdlbnQ6OmdhdGV3YXk=.1","YWdlbnQ6OmZyb250ZW5k.1"]},
       };
       // fetch services from api
       const services = await this.doRequest(s);
@@ -67,6 +78,63 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     });
     console.log(result);
     return result;
+  }
+
+   timeFormat (time: Date[]) {
+    let step: TimeType;
+    const unix = Math.round(time[1].getTime()) - Math.round(time[0].getTime());
+    if (unix <= 60 * 60 * 1000) {
+      step = TimeType.MINUTE_TIME;
+    } else if (unix <= 24 * 60 * 60 * 1000) {
+      step = TimeType.HOUR_TIME;
+    } else {
+      step = TimeType.DAY_TIME;
+    }
+    return { start: time[0], end: time[1], step };
+  };
+  dateFormatStep(date: Date, step: string, monthDayDiff?: boolean): string {
+    const year = date.getFullYear();
+    const monthTemp = date.getMonth() + 1;
+    let month = `${monthTemp}`;
+    if (monthTemp < 10) {
+      month = `0${monthTemp}`;
+    }
+    if (step === "MONTH" && monthDayDiff) {
+      return `${year}-${month}`;
+    }
+    const dayTemp = date.getDate();
+    let day = `${dayTemp}`;
+    if (dayTemp < 10) {
+      day = `0${dayTemp}`;
+    }
+    if (step === "DAY" || step === "MONTH") {
+      return `${year}-${month}-${day}`;
+    }
+    const hourTemp = date.getHours();
+    let hour = `${hourTemp}`;
+    if (hourTemp < 10) {
+      hour = `0${hourTemp}`;
+    }
+    if (step === "HOUR") {
+      return `${year}-${month}-${day} ${hour}`;
+    }
+    const minuteTemp = date.getMinutes();
+    let minute = `${minuteTemp}`;
+    if (minuteTemp < 10) {
+      minute = `0${minuteTemp}`;
+    }
+    if (step === "MINUTE") {
+      return `${year}-${month}-${day} ${hour}${minute}`;
+    }
+    const secondTemp = date.getSeconds();
+    let second = `${secondTemp}`;
+    if (secondTemp < 10) {
+      second = `0${secondTemp}`;
+    }
+    if (step === "SECOND") {
+      return `${year}-${month}-${day} ${hour}${minute}${second}`;
+    }
+    return "";
   }
 
   async testDatasource() {
